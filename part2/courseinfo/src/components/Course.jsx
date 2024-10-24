f76ea4a6bb85fd4b180e66f67906041568c3fae7
cj <cory.james.simpson.95@gmail.com>
2024-10-24 15:02:41 +1030

    Add exxercise 2.3

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,12 @@
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = ({course}) => 
     <div>
         <Header header={course.name} />
         <Content content={course.parts} />
+        <Total total={course.parts.reduce((total, curr) => total + curr.exercises, 0)} />
     </div>
 
   
