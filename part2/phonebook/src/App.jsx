41244673b6ee0729aa3ca7ac1353573d2480d510
cj <cory.james.simpson.95@gmail.com>
2024-10-24 18:25:18 +1030

    Add exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -8,6 +8,7 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -37,9 +38,25 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value.toLowerCase())
+  }
+  
+  const filterPersons = newFilter ?
+    persons.filter(person => person.name.toLowerCase().includes(newFilter)) :
+    persons
+
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>filter <input value={newFilter}
+        onChange={handleFilterChange}/></div>
+      </form>
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName}
@@ -55,7 +72,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
+        {filterPersons.map(person =>
           <Persons key={person.id} person={person} />
         )}
       </div>
